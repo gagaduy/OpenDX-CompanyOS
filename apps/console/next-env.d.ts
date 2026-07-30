f47ab740310322ac1c5a9e46e1683c490b613ed5
gagaduy <duongvanduy799@gmail.com>
2026-07-30 23:30:23 +0700

    chore(console): ignore generated type artifacts

--- apps/console/next-env.d.ts
+++ apps/console/next-env.d.ts
@@ -2,5 +2,8 @@
 /// <reference types="next/image-types/global" />
 import "./.next/types/routes.d.ts";
 
+// SPDX-FileCopyrightText: 2026 OpenDX CompanyOS contributors
+// SPDX-License-Identifier: Apache-2.0
+//
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
