037cb0dc9f8b800607cea61721f2705196276958
gagaduy <duongvanduy799@gmail.com>
2026-07-30 23:32:50 +0700

    chore(console): untrack generated next env

--- apps/console/env.d.ts
+++ apps/console/env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-import "./.next/types/routes.d.ts";
 
 // SPDX-FileCopyrightText: 2026 OpenDX CompanyOS contributors
 // SPDX-License-Identifier: Apache-2.0
-//
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
